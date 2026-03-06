8ac4c097920af52929c8e62ffe007b95a49f833d
Abdelaziz Mohamed <61994281+abdelazizhedeia@users.noreply.github.com>
2026-03-06 18:25:54 +0200

    Update group_manager.py

--- src/group_manager.py
+++ src/group_manager.py
@@ -1,1 +1,17 @@
+import os
 
+def add_group():
+    group = input("Enter group name: ")
+    os.system(f"sudo groupadd {group}")
+
+def add_user_to_group():
+    user = input("Username: ")
+    group = input("Group: ")
+    os.system(f"sudo usermod -aG {group} {user}")
+
+def delete_group():
+    group = input("Enter group name: ")
+    os.system(f"sudo groupdel {group}")
+
+def list_groups():
+    os.system("cut -d: -f1 /etc/group")
